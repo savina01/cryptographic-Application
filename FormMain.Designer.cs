d3c3e806f7fa20765fc85c298a7535b510e2c7c5
Savina Valchanova <vera0555@abv.bg>
2022-11-22 17:23:00 +0200

    Initial commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace cryptographicApplication
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.textBoxPlain = new System.Windows.Forms.TextBox();
+            this.textBoxKey1 = new System.Windows.Forms.TextBox();
+            this.textBoxChiper1 = new System.Windows.Forms.TextBox();
+            this.labelPlain = new System.Windows.Forms.Label();
+            this.labelKey1 = new System.Windows.Forms.Label();
+            this.labelCiper1 = new System.Windows.Forms.Label();
+            this.labelKey2 = new System.Windows.Forms.Label();
+            this.textBoxKey2 = new System.Windows.Forms.TextBox();
+            this.textBoxCiper2 = new System.Windows.Forms.TextBox();
+            this.labelCiper2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(432, 167);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 34);
+            this.buttonEncrypt.TabIndex = 10;
+            this.buttonEncrypt.Text = "encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // textBoxPlain
+            // 
+            this.textBoxPlain.Location = new System.Drawing.Point(23, 43);
+            this.textBoxPlain.Multiline = true;
+            this.textBoxPlain.Name = "textBoxPlain";
+            this.textBoxPlain.Size = new System.Drawing.Size(484, 48);
+            this.textBoxPlain.TabIndex = 7;
+            // 
+            // textBoxKey1
+            // 
+            this.textBoxKey1.Location = new System.Drawing.Point(23, 125);
+            this.textBoxKey1.Name = "textBoxKey1";
+            this.textBoxKey1.Size = new System.Drawing.Size(484, 22);
+            this.textBoxKey1.TabIndex = 8;
+            // 
+            // textBoxChiper1
+            // 
+            this.textBoxChiper1.Location = new System.Drawing.Point(23, 207);
+            this.textBoxChiper1.Multiline = true;
+            this.textBoxChiper1.Name = "textBoxChiper1";
+            this.textBoxChiper1.Size = new System.Drawing.Size(484, 64);
+            this.textBoxChiper1.TabIndex = 9;
+            // 
+            // labelPlain
+            // 
+            this.labelPlain.AutoSize = true;
+            this.labelPlain.Location = new System.Drawing.Point(20, 24);
+            this.labelPlain.Name = "labelPlain";
+            this.labelPlain.Size = new System.Drawing.Size(40, 16);
+            this.labelPlain.TabIndex = 11;
+            this.labelPlain.Text = "Plain:";
+            // 
+            // labelKey1
+            // 
+            this.labelKey1.AutoSize = true;
+            this.labelKey1.Location = new System.Drawing.Point(20, 106);
+            this.labelKey1.Name = "labelKey1";
+            this.labelKey1.Size = new System.Drawing.Size(40, 16);
+            this.labelKey1.TabIndex = 12;
+            this.labelKey1.Text = "Key1:";
+            // 
+            // labelCiper1
+            // 
+            this.labelCiper1.AutoSize = true;
+            this.labelCiper1.Location = new System.Drawing.Point(20, 188);
+            this.labelCiper1.Name = "labelCiper1";
+            this.labelCiper1.Size = new System.Drawing.Size(49, 16);
+            this.labelCiper1.TabIndex = 13;
+            this.labelCiper1.Text = "Ciper1:";
+            // 
+            // labelKey2
+            // 
+            this.labelKey2.AutoSize = true;
+            this.labelKey2.Location = new System.Drawing.Point(20, 286);
+            this.labelKey2.Name = "labelKey2";
+            this.labelKey2.Size = new System.Drawing.Size(40, 16);
+            this.labelKey2.TabIndex = 14;
+            this.labelKey2.Text = "Key2:";
+            // 
+            // textBoxKey2
+            // 
+            this.textBoxKey2.Location = new System.Drawing.Point(23, 305);
+            this.textBoxKey2.Name = "textBoxKey2";
+            this.textBoxKey2.Size = new System.Drawing.Size(484, 22);
+            this.textBoxKey2.TabIndex = 15;
+            // 
+            // textBoxCiper2
+            // 
+            this.textBoxCiper2.Location = new System.Drawing.Point(23, 360);
+            this.textBoxCiper2.Multiline = true;
+            this.textBoxCiper2.Name = "textBoxCiper2";
+            this.textBoxCiper2.Size = new System.Drawing.Size(484, 64);
+            this.textBoxCiper2.TabIndex = 16;
+            // 
+            // labelCiper2
+            // 
+            this.labelCiper2.AutoSize = true;
+            this.labelCiper2.Location = new System.Drawing.Point(20, 341);
+            this.labelCiper2.Name = "labelCiper2";
+            this.labelCiper2.Size = new System.Drawing.Size(49, 16);
+            this.labelCiper2.TabIndex = 17;
+            this.labelCiper2.Text = "Ciper2:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 492);
+            this.Controls.Add(this.labelCiper2);
+            this.Controls.Add(this.textBoxCiper2);
+            this.Controls.Add(this.textBoxKey2);
+            this.Controls.Add(this.labelKey2);
+            this.Controls.Add(this.labelCiper1);
+            this.Controls.Add(this.labelKey1);
+            this.Controls.Add(this.labelPlain);
+            this.Controls.Add(this.textBoxChiper1);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxKey1);
+            this.Controls.Add(this.textBoxPlain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cryptographic Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.TextBox textBoxPlain;
+        private System.Windows.Forms.TextBox textBoxKey1;
+        private System.Windows.Forms.TextBox textBoxChiper1;
+        private System.Windows.Forms.Label labelPlain;
+        private System.Windows.Forms.Label labelKey1;
+        private System.Windows.Forms.Label labelCiper1;
+        private System.Windows.Forms.Label labelKey2;
+        private System.Windows.Forms.TextBox textBoxKey2;
+        private System.Windows.Forms.TextBox textBoxCiper2;
+        private System.Windows.Forms.Label labelCiper2;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cryptographicApplication
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        public void PolyaplhabeticCiper()
+        {
+            //https://www.youtube.com/watch?v=Tpd_ebRNvf0
+            char[] cipher = new char[100];
+
+            string res = "";
+
+            int i = 0;
+            int j = 0;
+
+            string p = textBoxPlain.Text;
+            char[] plain = p.ToCharArray();
+            string k = textBoxKey1.Text;
+            char[] key = k.ToCharArray();
+
+            for (i = 0; i < plain.Length; i++)
+            {
+                //проверка за край на ключа и започване на неговото повтаряне, ако думата е свършила
+                if (j == key.Length)
+                    j = 0;
+
+                if ((plain[i] + (key[j] - 97)) > 122)
+                {
+                    //почти не се налага да влизаме тук
+                    cipher[i] = Convert.ToChar(plain[i] + ((key[j] - 97) - 26));
+                    j++;
+                }
+                else
+                {
+                    //намираме числото на c след като сме събрали числото на p с ключа
+                    cipher[i] = Convert.ToChar(plain[i] + (key[j] - 97));
+                    j++;
+                }
+            }
+            for (i = 0; i < plain.Length; i++)
+            {
+                res += cipher[i];
+            }
+            textBoxChiper1.Text = res;
+        }
+        public int[] GetShiftIndexes(string key)
+        {
+            //https://www.programmingalgorithms.com/algorithm/transposition-cipher/
+            int keyLength = key.Length;
+            int[] indexes =  new int[keyLength];
+            List<KeyValuePair<int,char>> sortedKey = new List<KeyValuePair<int, char>>();
+            int i;
+
+            for(i = 0; i < keyLength; i++)
+                sortedKey.Add(new KeyValuePair<int, char> (i, key[i]));
+            sortedKey.Sort(
+                delegate (KeyValuePair<int, char> pair1, KeyValuePair<int, char> pair2)
+                {
+                    return pair1.Value.CompareTo(pair2.Value);
+                });
+            for (i = 0; i < keyLength; i++)
+                indexes[sortedKey[i].Key] = i;
+            return indexes;
+
+        }
+        public void TranspositionCiper(string input, string key, char padChar)
+        {
+            input = (input.Length % key.Length == 0) ? input :
+                input.PadRight(input.Length - (input.Length % key.Length) + key.Length, padChar);
+
+            StringBuilder output = new StringBuilder();
+            string res = "";
+
+            int totalChars = input.Length;
+            int totalColumns = key.Length;
+            int totalRows = (int)Math.Ceiling((double)totalChars / totalColumns);
+            char[,] rowChars = new char[totalRows, totalColumns];
+            char[,] colChars = new char[totalColumns, totalRows];
+            char[,] sortedColChars = new char[totalColumns, totalRows];
+            int currentRow, currentColumn, i, j;
+            int[] shiftIndexes = GetShiftIndexes(key);
+
+            for (i = 0; i < totalChars; ++i)
+            {
+                currentRow = i / totalColumns;
+                currentColumn = i % totalColumns;
+                rowChars[currentRow, currentColumn] = input[i];
+            }
+
+            for (i = 0; i < totalRows; ++i)
+                for (j = 0; j < totalColumns; ++j)
+                    colChars[j, i] = rowChars[i, j];
+
+            for (i = 0; i < totalColumns; ++i)
+                for (j = 0; j < totalRows; ++j)
+                    sortedColChars[shiftIndexes[i], j] = colChars[i, j];
+
+            for(i=0; i < totalChars; ++i)
+            {
+                currentRow = i / totalRows;
+                currentColumn = i % totalRows;
+                output.Append(sortedColChars[currentRow, currentColumn]);
+            }
+            ;
+            res = output.ToString();
+            textBoxCiper2.Text = res;
+
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxPlain.Text) &&
+                string.IsNullOrEmpty(textBoxKey1.Text))
+            {
+                MessageBox.Show("error");
+            }
+            else
+                PolyaplhabeticCiper();
+            if (string.IsNullOrEmpty(textBoxChiper1.Text) &&
+                string.IsNullOrEmpty(textBoxKey2.Text))
+                MessageBox.Show("error2");
+            else
+                TranspositionCiper(textBoxChiper1.Text, textBoxKey2.Text, '-');
+
+        }
+    }
+}
